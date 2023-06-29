a92fd1b554a3969f421d30b3e8d36df7d24199a4
basilha <basilh@rubedoltd.com>
2023-06-29 09:37:44 +0300

    Created Objects for Masav Hiuvim (Get Payment) file
    Created new objects (getPaymentRecord, InstitutionGetPayment, and MasaVGetPayments) which create a masav receive payment file. The specifications for the file are found at  https://www.masav.co.il/media/1986/mifrat_hiuvim_msv.pdf
    
    The file is almost identical to the send payment, only that the 006 code in the payment record is changed to 504. Also, there is a small re-ordering of the summary line.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 "use strict";
 
 import InstitutionSendPayment from "./institutionSendPayment";
+import InstitutionGetPayment from "./institutionGetPayment";
 import { writeFileSync } from "fs";
 import Tools from "./tools";
 import SendPaymentsRecord from "./sendPaymentRecord";
+import GetPaymentsRecord from "./getPaymentRecord";
 
 class MasaVSendPayments {
   _intitutions: InstitutionSendPayment[] = [];
@@ -31,9 +33,38 @@ class MasaVSendPayments {
   }
 }
 
+class MasaVGetPayments {
+  _intitutions: InstitutionGetPayment[] = [];
+
+  addInstitution(intitution: InstitutionGetPayment) {
+    this._intitutions = [...this._intitutions, intitution];
+  }
+
+  addInstitutions(intitutions: InstitutionGetPayment[]) {
+    this._intitutions = [...this._intitutions, ...intitutions];
+  }
+
+  toBuffer() {
+    if (this._intitutions.length === 0)
+      throw new Error("There are no institutions");
+    let buffer: Buffer = Buffer.concat([
+      ...this._intitutions.map((institution) => institution.getBuffer()),
+      Tools.getEndOfFileRecord(),
+    ]);
+    return buffer;
+  }
+
+  saveFile(path: string) {
+    writeFileSync(path, this.toBuffer());
+  }
+}
+
 export {
   InstitutionSendPayment,
+  InstitutionGetPayment,
   MasaVSendPayments,
+  MasaVGetPayments,
   Tools,
   SendPaymentsRecord,
+  GetPaymentsRecord,
 };
